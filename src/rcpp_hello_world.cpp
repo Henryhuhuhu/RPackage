5a0a76acf0c3be97f9ffddfa1d6d7af0e5dbfd58
Henry Hu <henryhuhuhu@tamu.edu>
2022-11-20 19:15:38 -0600

    random number generator and low level primality check

--- src/rcpp_hello_world.cpp
+++ src/rcpp_hello_world.cpp
@@ -11,3 +11,37 @@ List rcpp_hello_world() {
 
     return z ;
 }
+
+int NumberGenerator(int n){
+  // random number of size n bits
+  int max = (long)powl(2, n) - 1;
+  int min = (long)powl(2, n - 1) + 1;
+  int number = min + (rand() % ( max - min + 1 ) );
+  return number;
+}
+
+bool LowLevelPrimeCheck(int number){
+  //first 100 prime numbers
+  int firstPrimes[100] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+                  31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+                  73, 79, 83, 89, 97, 101, 103, 107, 109, 113,
+                  127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
+                  179, 181, 191, 193, 197, 199, 211, 223, 227, 229,
+                  233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
+                  283, 293, 307, 311, 313, 317, 331, 337, 347, 349,
+                  353, 359, 367, 373, 379, 383, 389, 397, 401, 409,
+                  419, 421, 431, 433, 439, 443, 449, 457, 461, 463,
+                  467, 479, 487, 491, 499, 503, 509, 521, 523, 541};
+  for (int divisor : firstPrimes){
+    if (number % divisor == 0 && (divisor^2) <= number){
+      break;
+    }else{
+      return 1;
+    }
+  }
+  return 0;
+}
+
+bool HighLevelPrimeCheck(int number){
+  
+}
