aa895e52045e536dc62cc9631173e20813dc4790
Henry Hu <henryhuhuhu@tamu.edu>
2022-12-10 15:41:22 -0600

    Test file created

---
+++ R/tests.R
@@ -0,0 +1,18 @@
+Generaltest <- function(){
+  plaintext = "Hello World!"
+  primeList = RSAPrimeGenerate(6)
+  p = unlist(primeList)[1]
+  q = unlist(primeList)[2]
+  print(primeList)
+  keyList = RSAGenerateKey(p, q)
+  n = unlist(keyList)[1]
+  e = unlist(keyList)[2]
+  d = unlist(keyList)[3]
+  print(keyList)
+  ciphertext = RSAEncrypt(plaintext, e, n)
+  print(utf8ToInt(ciphertext))
+  print(ciphertext)
+  decoded = RSADecrypt(ciphertext, d, n)
+  print(utf8ToInt(decoded))
+  print(decoded)
+}
